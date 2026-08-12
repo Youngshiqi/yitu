c2269f2f5418da45d80ee24bd2b9e0758c6e09ca
yangshiqi <yshiqi408@gmial.com>
2026-08-12 17:09:34 +0800

    清理旧前端并完善 API 文档

--- frontend/src/api/payments.ts
+++
@@ -1,6 +0,0 @@
-import { api } from './client'
-
-export const paymentsApi = {
-  pay: (quoteId: string, shipmentId: string, amountCents: number) =>
-    api.post(`/payments/quotes/${quoteId}/pay`, { shipment_id: shipmentId, amount_cents: amountCents }),
-}
